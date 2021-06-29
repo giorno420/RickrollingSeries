6beff721228f7dd5f16e7e33509c643686d9fd83
Giorno420 <74859235+giorno420@users.noreply.github.com>
2021-06-29 20:12:52 +0100

    epic rickroll moment 😎

---
+++ python/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
