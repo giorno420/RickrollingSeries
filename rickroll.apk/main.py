c56eb97799868361e31eaf602ba606a197332a62
giorno420 <74859235+giorno420@users.noreply.github.com>
2021-06-29 20:32:52 +0100

    did stuff

---
+++ rickroll.apk/main.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+import webbrowser
+
+class RickRollApp(App):
+    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+RickRollApp().run()
--- rickroll.py/main.py
+++ rickroll.py/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
+    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
